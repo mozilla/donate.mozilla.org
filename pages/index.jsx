21f35220a280d2b661af414f539c872a20fb314a
Adam Lofting <contactme@adamlofting.com>
2015-07-28 15:45:04 +0100

    Switch to Universal Analytics, add Optimizely

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,15 +5,15 @@ var Index = React.createClass({
     return (
       <html>
         <head>
+          <script src="//cdn.optimizely.com/js/206878104.js"></script>
           <meta charSet="UTF-8"/>
           <meta name="viewport" content="width=device-width, initial-scale=1"/>
           <title>donate.mozilla.org | Give to Mozilla Today</title>
           <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
           <script src="https://sendto.mozilla.org/page/-/donation_form/js/parsley.min.js"></script>
-          <script src="/js/ga.js"></script>
-          <script src="//s.bsd.net/bsdaction/default/page/-/js/analytics/ga_integration-min.js"></script>
           <script src="https://js.stripe.com/v1/"></script>
           <script src="/js/stripe.js"></script>
+          <script src="/js/ga.js"></script>
           <script src="/js/analytics.js"></script>
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic"/>
           <link href="https://sendto.mozilla.org/page/-/donation_form/css/font-awesome.min.css" rel="stylesheet"/>

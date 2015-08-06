28cc25344cc96a34b7f9bde36dbb2b8e08bb3ef0
Mavis Ou <mmmavisss@gmail.com>
2015-08-06 10:58:56 -0700

    Fixes #158 - PayPal button component and Credit Card button component

---
+++ components/credit-card-button.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FormattedHTMLMessage, IntlMixin } from 'react-intl';
+
+var CreditCardButton = React.createClass({
+  mixins: [IntlMixin],
+  render: function() {
+    return (
+      <div className="half">
+        <input type="radio" name="payment-type" value="paypal" id="payment-paypal" data-parsley-group="page-2" data-parsley-multiple="payment-type" data-parsley-errors-container="#payment-type-error-msg" data-parsley-required/>
+        <label htmlFor="payment-paypal">
+          <div className="row payment-logos paypal-logo">
+            <p>&nbsp;</p>
+          </div>
+          <div className="row medium-label-size">PayPal</div>
+        </label>
+      </div>
+    );
+  }
+})
+
+module.exports = CreditCardButton;
---
+++ components/paypal-button.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FormattedHTMLMessage, IntlMixin } from 'react-intl';
+
+var PaypalButton = React.createClass({
+  mixins: [IntlMixin],
+  render: function() {
+    return (
+      <div className="half">
+        <input type="radio" name="payment-type" value="cc" id="payment-cc" data-parsley-group="page-2" data-parsley-multiple="payment-type" data-parsley-errors-container="#payment-type-error-msg" data-parsley-required/>
+        <label id="payment-cc-label" htmlFor="payment-cc">
+          <div className="row payment-logos credit-card-logos">
+            <p>&nbsp;</p>
+          </div>
+          <div className="row medium-label-size">{this.getIntlMessage('credit_card')}</div>
+        </label>
+      </div>
+    );
+  }
+})
+
+module.exports = PaypalButton;

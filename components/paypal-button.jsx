6d0cbcb3507edb9aa157d8ad1e4585fc59e23577
Scott Downe <scott.downe@gmail.com>
2015-08-10 03:04:35 -0400

    some fixes I didn't see initially

--- components/paypal-button.jsx
+++ components/paypal-button.jsx
@@ -10,17 +10,17 @@ module.exports = React.createClass({
   render: function() {
     return (
       <div className="half paypal-button">
-          <input type="radio" name="payment-type" value="paypal" id="payment-paypal" data-parsley-group="page-2" data-parsley-multiple="payment-type" data-parsley-errors-container="#payment-type-error-msg" data-parsley-required/>
-          <label className="payment-submit-button" htmlFor="payment-paypal">
-            <button className="submit-button" type="submit" onClick={this.onClick} onSubmit={this.onSubmit} formAction="/paypal">
-              <div className="row payment-logos paypal-logo">
-                <p>&nbsp;</p>
-              </div>
-              <div className="row medium-label-size">PayPal</div>
-            </button>
-          </label>
-          <input type="hidden" name="paypal_locale_code" value="US"/>
-          <input type="hidden" name="paypal_currency_code" value="USD"/>
+        <input type="radio" name="payment-type" value="paypal" id="payment-paypal" data-parsley-group="page-2" data-parsley-multiple="payment-type" data-parsley-errors-container="#payment-type-error-msg" data-parsley-required/>
+        <label className="payment-submit-button" htmlFor="payment-paypal">
+          <button className="submit-button" type="submit" onClick={this.onClick} onSubmit={this.onSubmit} formAction="/paypal">
+            <div className="row payment-logos paypal-logo">
+              <p>&nbsp;</p>
+            </div>
+            <div className="row medium-label-size">PayPal</div>
+          </button>
+        </label>
+        <input type="hidden" name="paypal_locale_code" value="US"/>
+        <input type="hidden" name="paypal_currency_code" value="USD"/>
       </div>
     );
   }

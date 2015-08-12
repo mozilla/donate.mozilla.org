644737d4937f92bd7e525aa66ba2bd2dcaf6a2ed
Ali Al Dallal <ali@alicoding.com>
2015-08-12 09:59:13 -0400

    Fix issue #107 - Improve shared page for non en-US locales
    closes #107

---
+++ components/social.jsx
@@ -0,0 +1,31 @@
+var React = require('react');
+
+module.exports = React.createClass({
+  mixins: [require('react-intl').IntlMixin],
+  render: function() {
+    return (
+      <div className="share-page">
+        <div className="container">
+          <h2>{this.getIntlMessage('share')}</h2>
+          <div className="row">
+            <div className="half" id="facebook">
+              <a href="http://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdonate.mozilla.org">
+                <i className="fa fa-facebook fa-5x"></i>
+
+                <p>facebook</p>
+              </a>
+            </div>
+            <div className="half" id="twitter">
+              <a href="http://twitter.com/share?url=https%3A%2F%2Fdonate.mozilla.org&amp;text=I%20donated%20to%20%40mozilla%20today%20because%20I%20%23lovetheweb.%20Join%20me%20and%20help%20build%20%2B%20protect%20the%20Web%20forever%3A">
+                <i className="fa fa-twitter fa-5x"></i>
+
+                <p>twitter</p>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+});
--- pages/share.jsx
+++ pages/share.jsx
@@ -1,39 +1,21 @@
 import React  from 'react';
 import Footer from '../components/footer.jsx';
 import Header from '../components/header.jsx';
+import Social from '../components/social.jsx';
 
 var ThankYou = React.createClass({
+  mixins: [require('react-intl').IntlMixin],
   render: function() {
     return (
-      <div className="share-page">
-        <div className="mozilla-eoy-donation">
-          <Header/>
-          <div>
-            <div id="header-copy">
-              <h1>Tell your friends</h1>
-              <h4>We need your help to spread the word.</h4>
-            </div>
-            <div className="container">
-              <h2>Share</h2>
-              <div className="row">
-                <div className="half" id="facebook">
-                  <a href="http://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdonate.mozilla.org">
-                    <i className="fa fa-facebook fa-5x"></i>
-
-                    <p>facebook</p>
-                  </a>
-                </div>
-                <div className="half" id="twitter">
-                  <a href="http://twitter.com/share?url=https%3A%2F%2Fdonate.mozilla.org&amp;text=I%20donated%20to%20%40mozilla%20today%20because%20I%20%23lovetheweb.%20Join%20me%20and%20help%20build%20%2B%20protect%20the%20Web%20forever%3A">
-                    <i className="fa fa-twitter fa-5x"></i>
-
-                    <p>twitter</p>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <Footer/>
+      <div className="mozilla-eoy-donation">
+        <Header/>
+        <div>
+          <div id="header-copy">
+            <h1>Tell your friends</h1>
+            <h4>We need your help to spread the word.</h4>
           </div>
+          <Social/>
+          <Footer/>
         </div>
       </div>
     );

993bc0e1066100214e51a1f4cda1baff17bf48a8
Ali Al Dallal <ali@alicoding.com>
2015-08-26 15:58:07 -0400

    Change from YAML to PO file

---
+++ scripts/po2json.js
@@ -0,0 +1,37 @@
+var config = require('../intl-config.json');
+var po = require('node-po');
+var write = require('fs-writefile-promise');
+var path = require('path');
+
+function writeFile(entries) {
+  entries.reduce(function (prevEntry, entry) {
+    write(path.join(process.cwd(), config.dest, entry.locale + '.json'), JSON.stringify(entry.content, null, 2), 'utf-8')
+    .then(function(filename) {
+      console.log('Done writing: ' + filename);
+    }).catch(function(e) {
+      console.log(e);
+    });
+  }, {});
+}
+
+function getJSON(locale) {
+  return new Promise(function(resolve) {
+    po.load(path.join(process.cwd(), config.src, locale + '.po'), function(_po) {
+      var json = {};
+      _po.items.forEach(function(entry) {
+        json[entry.msgid] = entry.msgstr[0];
+      });
+      resolve({content: json, locale: locale});
+    });
+  });
+}
+
+function processMessageFiles(locales) {
+  return Promise.all(locales.map(getJSON));
+}
+
+processMessageFiles(config.supportedLocales)
+.then(writeFile).catch(function (err) {
+  console.error(err);
+  throw err;
+});

7f5220f61a140dce8af5e901ccc7281fa6e934bb
Scott Downe <scott.downe@gmail.com>
2015-09-08 09:37:36 -0400

    single quotes

--- data/currencies.js
+++ data/currencies.js
@@ -3,132 +3,132 @@ module.exports = {
     code: 'usd',
     minAmount: '2',
     symbol: '$',
-    presets: ["20", "10", "5", "3"]
+    presets: ['20', '10', '5', '3']
   },
   'php': {
     code: 'php',
     minAmount: '90',
     symbol: '₱',
-    presets: ["400", "200", "100", "90"]
+    presets: ['400', '200', '100', '90']
   },
   'pln': {
     code: 'pln',
     minAmount: '7',
     symbol: 'zł',
-    presets: ["40", "20", "10", "7"]
+    presets: ['40', '20', '10', '7']
   },
   'rub': {
     code: 'rub',
     minAmount: '100',
     symbol: 'руб',
-    presets: ["600", "300", "150", "100"]
+    presets: ['600', '300', '150', '100']
   },
   'sek': {
     code: 'sek',
     minAmount: '15',
     symbol: 'kr',
-    presets: ["80", "40", "20", "15"]
+    presets: ['80', '40', '20', '15']
   },
   'thb': {
     code: 'thb',
     minAmount: '70',
     symbol: '฿',
-    presets: ["400", "200", "100", "70"]
+    presets: ['400', '200', '100', '70']
   },
   'twd': {
     code: 'twd',
     minAmount: '62',
     symbol: 'NT$',
-    presets: ["400", "200", "100", "70"]
+    presets: ['400', '200', '100', '70']
   },
   'nzd': {
     code: 'nzd',
     minAmount: '3',
     symbol: '$',
-    presets: ["20", "10", "5", "3"]
+    presets: ['20', '10', '5', '3']
   },
   'huf': {
     code: 'huf',
     minAmount: '490',
     symbol: 'Ft',
-    presets: ["2800", "1400", "700", "500"]
+    presets: ['2800', '1400', '700', '500']
   },
   'ils': {
     code: 'ils',
     minAmount: '8',
     symbol: '₪',
-    presets: ["60", "30", "15", "10"]
+    presets: ['60', '30', '15', '10']
   },
   'jpy': {
     code: 'jpy',
     minAmount: '240',
     symbol: '¥',
-    presets: ["1200", "600", "300", "250"]
+    presets: ['1200', '600', '300', '250']
   },
   'mxn': {
     code: 'mxn',
     minAmount: '30',
     symbol: '$',
-    presets: ["160", "80", "40", "30"]
+    presets: ['160', '80', '40', '30']
   },
   'nok': {
     code: 'nok',
     minAmount: '15',
     symbol: 'kr',
-    presets: ["80", "40", "20", "15"]
+    presets: ['80', '40', '20', '15']
   },
   'aud': {
     code: 'aud',
     minAmount: '2',
     symbol: '$',
-    presets: ["20", "10", "5", "3"]
+    presets: ['20', '10', '5', '3']
   },
   'brl': {
     code: 'brl',
     minAmount: '5',
     symbol: 'R$',
-    presets: ["40", "20", "10", "5"]
+    presets: ['40', '20', '10', '5']
   },
   'cad': {
     code: 'cad',
     minAmount: '2',
     symbol: '$',
-    presets: ["20", "10", "5", "3"]
+    presets: ['20', '10', '5', '3']
   },
   'chf': {
     code: 'chf',
     minAmount: '2',
     symbol: 'CHF',
-    presets: ["20", "10", "5", "3"]
+    presets: ['20', '10', '5', '3']
   },
   'czk': {
     code: 'czk',
     minAmount: '45',
     symbol: 'Kč',
-    presets: ["240", "120", "60", "45"]
+    presets: ['240', '120', '60', '45']
   },
   'dkk': {
     code: 'dkk',
     minAmount: '12',
     symbol: 'kr',
-    presets: ["80", "40", "20", "12"]
+    presets: ['80', '40', '20', '12']
   },
   'eur': {
     code: 'eur',
     minAmount: '2',
     symbol: '€',
-    presets: ["20", "10", "5", "3"]
+    presets: ['20', '10', '5', '3']
   },
   'gbp': {
     code: 'gbp',
     minAmount: '1',
     symbol: '£',
-    presets: ["20", "10", "5", "3"]
+    presets: ['20', '10', '5', '3']
   },
   'hkd': {
     code: 'hkd',
     minAmount: '15',
     symbol: '$',
-    presets: ["80", "40", "20", "15"]
+    presets: ['80', '40', '20', '15']
   }
 };

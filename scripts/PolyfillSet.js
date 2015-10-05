696cede6d3a05cd7fe0c22740ece4ee2354ab2c9
Ali Al Dallal <ali@alicoding.com>
2015-10-05 11:28:32 -0400

    Fixes

---
+++ scripts/PolyfillSet.js
@@ -0,0 +1,29 @@
+var PolyfillSet = function(polyfillset) {
+  this.data = polyfillset;
+};
+
+PolyfillSet.prototype.stringify = function() {
+  return Object.keys(this.data).map(function(featureName) {
+    var flags = this.data[featureName].flags;
+    return featureName + (flags.length ? '|' + flags.join('|') : '');
+  }, this).join(',');
+};
+PolyfillSet.prototype.get = function() {
+  return this.data;
+};
+
+
+PolyfillSet.fromQueryParam = function(polyfillList, additionalFlags) {
+  var list = polyfillList.split(',').filter(function(x) { return x.length; });
+  additionalFlags = additionalFlags || [];
+
+  return new PolyfillSet(list.sort().reduce(function parsePolyfillInfo(obj, name) {
+    var nameAndFlags = name.split('|');
+    obj[nameAndFlags[0]] = {
+      flags:   nameAndFlags.slice(1).concat(additionalFlags)
+    };
+    return obj;
+  }, {}));
+};
+
+module.exports = PolyfillSet;

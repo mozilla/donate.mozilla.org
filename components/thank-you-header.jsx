31cb205ab1399c09088e28c4f1c25d4dc2bf1e58
Scott Downe <scott.downe@gmail.com>
2015-10-22 12:51:45 -0400

    fix up some l10n formatting stuff

--- components/thank-you-header.jsx
+++ components/thank-you-header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IntlMixin } from 'react-intl';
+import { IntlMixin, FormattedHTMLMessage } from 'react-intl';
 
 module.exports = React.createClass({
   mixins: [IntlMixin],

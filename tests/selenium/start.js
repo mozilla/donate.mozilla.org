182c40c55e36d7e626c1780d2ec2c95c54f62ebe
Ali Al Dallal <ali@alicoding.com>
2015-10-26 16:26:05 -0400

    Add more selenium tests

--- tests/selenium/start.js
+++ tests/selenium/start.js
@@ -1,14 +1,34 @@
 var server = require('../../server');
-server.start(function() {
-  var webdriver = require('selenium-webdriver'),
-    By = require('selenium-webdriver').By;
-
-  var driver = new webdriver.Builder().
+var webdriver = require('selenium-webdriver'),
+  By = require('selenium-webdriver').By;
+var driver = new webdriver.Builder().
   withCapabilities(webdriver.Capabilities.firefox()).
   build();
 
-  driver.manage().timeouts().implicitlyWait(30000);
-  driver.get('http://localhost:3000/de/');
+server.start(function() {
+  test(false, 'http://localhost:3000/de/');
+  driver.wait(function() {
+    return driver.getCurrentUrl().then(function(url) {
+      return url.indexOf('http://localhost:3000/de/thank-you/') === 0;
+    });
+  }).then(function() {
+    test(true, 'http://localhost:3000/de/?test=signup-test');
+    driver.wait(function() {
+      return driver.getCurrentUrl().then(function(url) {
+        return url.indexOf('http://localhost:3000/de/share/') === 0;
+      });
+    }).then(function() {
+      server.stop(function() {
+        driver.quit();
+      });
+    });
+  });
+});
+
+function test(noProvince, url) {
+  var country = 'Canada';
+  driver.manage().timeouts().implicitlyWait(3000);
+  driver.get(url);
   driver.findElement(By.id('amount-other-input')).sendKeys('10');
   driver.findElement(By.css('.page-active .next-button')).click();
   driver.findElement(By.id('payment-cc-label')).click();
@@ -25,25 +45,24 @@ server.start(function() {
   driver.findElement(By.name('firstname')).sendKeys('testname');
   driver.findElement(By.name('lastname')).clear();
   driver.findElement(By.name('lastname')).sendKeys('lastname');
-  driver.findElement(By.name('country')).sendKeys('Canada');
+  if (noProvince) {
+    country = 'Chile';
+  }
+  driver.findElement(By.name('country')).sendKeys(country);
   driver.findElement(By.name('addr1')).clear();
   driver.findElement(By.name('addr1')).sendKeys('test address');
   driver.findElement(By.name('city')).clear();
   driver.findElement(By.name('city')).sendKeys('testcity');
   driver.findElement(By.name('zip')).clear();
   driver.findElement(By.name('zip')).sendKeys('m5g 3u7');
-  driver.findElement(By.id('wsstate_cd')).sendKeys('Ontario');
+  if (!noProvince) {
+    driver.findElement(By.id('wsstate_cd')).sendKeys('Ontario');
+  }
   driver.findElement(By.name('email')).clear();
   driver.findElement(By.name('email')).sendKeys('test@email.com');
-  driver.findElement(By.id('legalConfirm')).click();
+  driver.findElement(By.id('privacy-policy-checkbox')).click();
+  if (noProvince) {
+    driver.findElement(By.id('signup-checkbox')).click();
+  }
   driver.findElement(By.css('.submit-btn')).click();
-  driver.wait(function() {
-    return driver.getCurrentUrl().then(function(url) {
-      return url.indexOf('http://localhost:3000/de/thank-you/') === 0;
-    });
-  }, 5000).then(function() {
-    server.stop(function() {
-      driver.quit();
-    });
-  });
-});
+}

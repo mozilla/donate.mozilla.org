77e19adab7093bfe8ae841c35a4d5f793a3e5ce8
Scott Downe <scott.downe@gmail.com>
2015-10-29 15:31:19 -0400

    fix failing test

--- scripts/dispatcher.js
+++ scripts/dispatcher.js
@@ -1,12 +1,12 @@
 module.exports = {
-  fire: function (eventName, eventData) {
+  fire: function(eventName, eventData) {
     var customEvent = new CustomEvent(eventName, {
       detail: eventData
     });
 
     window.dispatchEvent(customEvent);
   },
-  on: function (eventName, callback) {
+  on: function(eventName, callback) {
     window.addEventListener(eventName, function(e) {
       callback.call(this, e.detail);
     });

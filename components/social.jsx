b7f2ba7fc169697429387770382cbdae3cd50c77
Jon Buckley <jon@jbuckley.ca>
2015-11-20 14:33:55 -0500

    URI encode Twitter share text

--- components/social.jsx
+++ components/social.jsx
@@ -4,7 +4,7 @@ module.exports = React.createClass({
   mixins: [require('react-intl').IntlMixin],
   render: function() {
     var appURL = process.env.APPLICATION_URI;
-    var twitterShareURL = 'http://twitter.com/share?url=' + appURL +'/' + this.props.language + '/&text=' + this.getIntlMessage('i_donated_to_mozilla');
+    var twitterShareURL = 'http://twitter.com/share?url=' + appURL +'/' + this.props.language + '/&text=' + encodeURIComponent(this.getIntlMessage('i_donated_to_mozilla'));
     var facebookShareURL = 'http://www.facebook.com/sharer/sharer.php?u=' + appURL + '/' + this.props.language + '/';
     return (
       <div className="share-page">

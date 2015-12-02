3e69412bddb2d723a63ebb020feec869174393d1
Christopher De Cairos <cade@mozillafoundation.org>
2015-12-02 10:40:25 -0500

    no asnyc attribute

--- pages/sequential.jsx
+++ pages/sequential.jsx
@@ -249,7 +249,7 @@ module.exports = React.createClass({
         </div>
         <SmallPrint stripeNotice={!this.state.hideCreditCardDetails}/>
         <Footer/>
-        <script async src="https://js.stripe.com/v2/"></script>
+        <script src="https://js.stripe.com/v2/"></script>
       </div>
     );
   }

f2390924a9add978a0f1e109c0ffe1ee5af6ff01
Scott Downe <scott.downe@gmail.com>
2015-12-03 15:57:24 -0500

    merge about and one page into a single form component.

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,73 +1,10 @@
 import React from 'react';
 import Footer from '../components/footer.jsx';
 import SmallPrint from '../components/small-print.jsx';
-import SectionHeading from '../components/section-heading.jsx';
-import CurrencyDropdown from '../components/currency-dropdown.jsx';
-import {ErrorListener} from '../components/error.jsx';
+import SingleForm from '../components/single-form.jsx';
 
-import AmountButtons from '../components/amount-buttons.jsx';
-import Frequency from '../components/donation-frequency.jsx';
-import {PayPalButton, StripeButton} from '../components/payment-options.jsx';
-
-import SubmitButton from '../components/submit-button.jsx';
-import DonateButton from '../components/donate-button.jsx';
-import {PrivacyPolicyCheckbox} from '../components/checkbox.jsx';
-
-var SingleForm = React.createClass({
-  mixins: [require('react-intl').IntlMixin, require('../mixins/form.jsx')],
-  renderPaymentOptions: function() {
-    if (!this.state.currency.disabled) {
-      return (
-        <span>
-          <SectionHeading>
-            <h4>{this.getIntlMessage("choose_payment")}</h4>
-            <p id="secure-label"><i className="fa fa-lock"></i>{this.getIntlMessage('secure')}</p>
-          </SectionHeading>
-          <div className="row">
-            <PrivacyPolicyCheckbox name="privacyPolicy"/>
-          </div>
-          <ErrorListener errors={["cardNumber", "cvc", "expMonth", "expYear"]}/>
-          <StripeButton
-            submit={["frequency", "amount"]}
-            validate={["amount", "privacyPolicy"]}
-            onSubmit={this.stripeCheckout}
-          />
-          <PayPalButton
-            submitting={this.state.submitting}
-            submit={["frequency", "amount"]}
-            validate={["amount", "privacyPolicy"]}
-            onSubmit={this.paypal}
-          />
-        </span>
-      );
-    } else if (this.state.currency.disabled === "paypal") {
-      return (
-        <span className="paypal-disabled">
-          <SectionHeading>
-            <h2>{this.getIntlMessage("credit_card")}</h2>
-            <p id="secure-label">
-              <i className="fa fa-lock"></i>{this.getIntlMessage('secure')}
-            </p>
-            <div className="row payment-logos credit-card-logos">
-              <p>&nbsp;</p>
-            </div>
-          </SectionHeading>
-          <div className="row">
-            <PrivacyPolicyCheckbox name="privacyPolicy"/>
-          </div>
-          <ErrorListener errors={["cardNumber", "cvc", "expMonth", "expYear"]}/>
-          <SubmitButton
-            submitting={this.state.submitting}
-            submit={["amount", "frequency"]}
-            validate={["amount", "privacyPolicy"]}
-            onSubmit={this.stripeCheckout}
-          >
-            <DonateButton currency={this.state.currency}/>
-          </SubmitButton>
-        </span>
-      );
-    }
-  },
+module.exports = React.createClass({
+  mixins: [require('react-intl').IntlMixin],
   render: function() {
     var className = "row additional-info-container new-flow-test";
     if (this.props.test) {
@@ -83,21 +20,14 @@ var SingleForm = React.createClass({
               <span>{this.getIntlMessage('additional_info')}</span>
             </div>
           </div>
-          <div className="container">
-            <SectionHeading>
-              <h2>
-                {this.getIntlMessage("donate_now")}
-                <span className="right">
-                  <CurrencyDropdown/>
-                </span>
-              </h2>
-            </SectionHeading>
-            <AmountButtons name="amount" locale={this.props.locales[0]}/>
-            <Frequency name="frequency"/>
-            <div className="payment-section">
-              {this.renderPaymentOptions()}
-            </div>
-          </div>
+          <SingleForm
+            currency={this.props.currency}
+            presets={this.props.presets}
+            amount={this.props.amount}
+            frequency={this.props.frequency}
+            country={this.props.country}
+            locales={this.props.locales}
+          />
         </div>
         <SmallPrint stripeNotice={true} />
         <Footer/>
@@ -105,5 +35,3 @@ var SingleForm = React.createClass({
     );
   }
 });
-
-module.exports = SingleForm;

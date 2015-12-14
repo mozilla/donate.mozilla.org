dcefcf9e902530de5995672abc78dda8d654adca
Ali Al Dallal <ali@alicoding.com>
2015-12-14 17:37:23 -0500

    Add test by jbuck

---
+++ tests/http/paypal.test.js
@@ -0,0 +1,26 @@
+var server = require('../../server');
+var should = require('should');
+
+describe('/api/paypal', () => {
+  var instance = server({ useDomains: false });
+
+  it('should return 400 when invalid currency is used', (done) => {
+    instance.inject({
+      method: 'POST',
+      url: '/api/paypal',
+      payload: {
+        frequency: 'onetime',
+        description: 'Test',
+        amount: 3,
+        locale: 'en-US',
+        currency: 'buck'
+      }
+    }, (response) => {
+      should(response.statusCode).equal(400);
+      should(response.result.validation.source).equal('payload');
+      should(response.result.validation.keys).deepEqual(['currency']);
+
+      done();
+    });
+  });
+});

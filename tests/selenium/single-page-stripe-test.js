5c1b52f3cdffdaa1dfa0ec526494bf0ab315bc89
Scott Downe <scott.downe@gmail.com>
2015-12-15 16:13:58 -0500

    fixup tests after rebase and add new test

--- tests/selenium/single-page-stripe-test.js
+++ tests/selenium/single-page-stripe-test.js
@@ -2,7 +2,7 @@ module.exports = function(driver, By, done) {
   driver.get('http://localhost:3000/en-US/');
   driver.findElement(By.id('amount-other-input')).sendKeys('10');
   driver.findElement(By.id('privacy-policy-checkbox')).click();
-  driver.findElement(By.css('.payment-cc-label')).click();
+  driver.findElement(By.css('.frequency-move-baseline .payment-cc-label')).click();
 
   driver.switchTo().frame(driver.findElement(By.css(".stripe_checkout_app")));
   var windowHandle = driver.getWindowHandle();
@@ -54,7 +54,7 @@ module.exports = function(driver, By, done) {
   driver.get('http://localhost:3000/en-US/?billingAddress=partial');
   driver.findElement(By.id('amount-other-input')).sendKeys('10');
   driver.findElement(By.id('privacy-policy-checkbox')).click();
-  driver.findElement(By.css('.payment-cc-label')).click();
+  driver.findElement(By.css('.frequency-move-baseline .payment-cc-label')).click();
 
   driver.switchTo().frame(driver.findElement(By.css(".stripe_checkout_app")));
   windowHandle = driver.getWindowHandle();
@@ -99,7 +99,7 @@ module.exports = function(driver, By, done) {
   driver.get('http://localhost:3000/pa-IN/');
   driver.findElement(By.id('amount-other-input')).sendKeys('200');
   driver.findElement(By.id('privacy-policy-checkbox')).click();
-  driver.findElement(By.css('.submit-btn')).click();
+  driver.findElement(By.css('.frequency-move-baseline .submit-btn')).click();
 
   driver.switchTo().frame(driver.findElement(By.css(".stripe_checkout_app")));
   windowHandle = driver.getWindowHandle();
@@ -142,6 +142,52 @@ module.exports = function(driver, By, done) {
         result = false;
       }
       if (url.indexOf('&country=Canada') === -1) {
+        result = false;
+      }
+      return result;
+    });
+  });
+
+  driver.get('http://localhost:3000/pa-IN/?test=frequency-move-test&billingAddress=partial');
+  driver.findElement(By.css('.frequency-move .monthly-payment')).click();
+  driver.findElement(By.id('amount-other-input')).sendKeys('200');
+  driver.findElement(By.id('privacy-policy-checkbox')).click();
+  driver.findElement(By.css('.frequency-move-test .submit-btn')).click();
+
+  driver.switchTo().frame(driver.findElement(By.css(".stripe_checkout_app")));
+  windowHandle = driver.getWindowHandle();
+  driver.wait(function() {
+    return driver.switchTo().activeElement().getAttribute("id").then(function(id) {
+      return id === "email";
+    });
+  });
+  driver.findElement(By.id('email')).sendKeys('send-donation@test.com');
+  driver.findElement(By.id('card_number')).sendKeys('4242');
+  driver.findElement(By.id('card_number')).sendKeys('4242');
+  driver.findElement(By.id('card_number')).sendKeys('4242');
+  driver.findElement(By.id('card_number')).sendKeys('4242');
+  driver.findElement(By.id('cc-exp')).sendKeys('01');
+  driver.findElement(By.id('cc-exp')).sendKeys('16');
+  driver.findElement(By.id('cc-csc')).sendKeys('123');
+  driver.findElement(By.id('billing-zip')).sendKeys('a');
+  driver.findElement(By.id('submitButton')).click();
+  driver.switchTo().window(windowHandle);
+  driver.wait(function() {
+    return driver.getCurrentUrl().then(function(url) {
+      var result = true;
+      if (url.indexOf('http://localhost:3000/pa-IN/thank-you/') === -1) {
+        result = false;
+      }
+      if (url.indexOf('&str_amount=20000') === -1) {
+        result = false;
+      }
+      if (url.indexOf('&str_currency=inr') === -1) {
+        result = false;
+      }
+      if (url.indexOf('&str_frequency=monthly') === -1) {
+        result = false;
+      }
+      if (url.indexOf('&email=send-donation%40test.com') === -1) {
         result = false;
       }
       return result;

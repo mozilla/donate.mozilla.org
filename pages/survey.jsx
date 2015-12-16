f18db5aaf41be7c8cf0212f7d48f09865553e753
Adam Lofting <contactme@adamlofting.com>
2015-12-16 14:34:05 +0000

    Remove seperate component

--- pages/survey.jsx
+++ pages/survey.jsx
@@ -1,21 +1,23 @@
 import React  from 'react';
 import Footer from '../components/footer.jsx';
 import Header from '../components/header.jsx';
-import SurveyGizmo from '../components/surveygizmo.jsx';
 import { IntlMixin } from 'react-intl';
 
 var ThankYou = React.createClass({
   mixins: [IntlMixin],
   render: function() {
     var className = "row new-flow-test";
-    var language = this.props.locales[0];
     if (this.props.test) {
       className += " " + this.props.test;
     }
     return (
       <div className={className}>
-        <Header />
-        <SurveyGizmo language={language}/>
+        <Header/>
+        <div className="survey-page">
+          <div className="container">
+            <iframe src="https://www.surveygizmo.com/s3/2472227/Mozilla-Donor-Survey" frameBorder="0" width="100%" height="600"></iframe>
+          </div>
+        </div>
         <Footer/>
       </div>
     );

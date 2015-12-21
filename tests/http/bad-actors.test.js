9aa10132898ba3e9415ae15d46dd3291637fb818
Jon Buckley <jon@jbuckley.ca>
2015-12-21 13:22:31 -0500

    Check error message too

--- tests/http/bad-actors.test.js
+++ tests/http/bad-actors.test.js
@@ -8,6 +8,7 @@ describe('bad actors', () => {
         url: '/����%20wwwroot.rar'
       }, (response) => {
         should(response.statusCode).equal(400);
+        should(response.result.message).equal('Location cannot contain or convert into non-ascii characters');
 
         done();
       });

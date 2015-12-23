1d577d2d81e9f21f5f145ef4e2adf926652a29ec
Ali Al Dallal <ali@alicoding.com>
2015-12-23 11:05:33 -0500

    Remove two sequential tests

--- tests/selenium/start.js
+++ tests/selenium/start.js
@@ -1,14 +1,10 @@
 var testRunner = require('./test-runner');
-var sequentialStripe = require('./sequential-stripe-test.js');
-var sequentialPaypal = require('./sequential-paypal-test.js');
 var singlePageStripe = require('./single-page-stripe-test.js');
 var singlePagePaypal = require('./single-page-paypal-test.js');
 var simplePaypal = require('./simple-paypal-test.js');
 var thankYou = require('./thank-you-test.js');
 
 testRunner([
-  sequentialStripe,
-  sequentialPaypal,
   singlePageStripe,
   singlePagePaypal,
   simplePaypal,

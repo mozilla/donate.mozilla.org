221d0c9cf6d4ceedd69a1f97d6dd6eeb21786007
Ali Al Dallal <ali@alicoding.com>
2016-03-01 13:02:58 -0500

    Add selenium tests

--- tests/selenium/start.js
+++ tests/selenium/start.js
@@ -3,6 +3,8 @@ var sequentialStripe = require('./sequential-stripe-test.js');
 var sequentialPaypal = require('./sequential-paypal-test.js');
 var singlePageStripe = require('./single-page-stripe-test.js');
 var singlePagePaypal = require('./single-page-paypal-test.js');
+var singlePageThunderbirdStripe = require('./single-page-thunderbird-stripe-test.js');
+var singlePageThunderbirdPayPal = require('./single-page-thunderbird-paypal-test.js');
 var simplePaypal = require('./simple-paypal-test.js');
 var thankYou = require('./thank-you-test.js');
 
@@ -11,6 +13,8 @@ testRunner([
   sequentialPaypal,
   singlePageStripe,
   singlePagePaypal,
+  singlePageThunderbirdPayPal,
+  singlePageThunderbirdStripe,
   simplePaypal,
   thankYou
 ]);

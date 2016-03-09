5a7140c1be619016f6eaa79ac02448f5173f1a13
ScottDowne <scott.downe@gmail.com>
2016-03-09 00:07:58 -0500

    trying to optimize webpack

--- data/pages.js
+++ data/pages.js
@@ -4,20 +4,10 @@ var pages = {
     path: '/:locale/',
     handler: require('../pages/one-page.jsx')
   },
-  '/partial-page/': {
-    name: 'partial-page',
-    path: '/:locale/partial-page/?',
-    handler: require('../pages/partial-page.jsx')
-  },
   '/about/': {
     name: 'about',
     path: '/:locale/about/?',
     handler: require('../pages/about.jsx')
-  },
-  '/one-page-monthly/': {
-    name: 'one-page-monthly',
-    path: '/:locale/one-page-monthly/?',
-    handler: require('../pages/one-page-monthly.jsx')
   },
   '/thank-you/': {
     name: 'thank-you',
@@ -29,30 +19,10 @@ var pages = {
     path: '/:locale/share/?',
     handler: require('../pages/share.jsx')
   },
-  '/survey/': {
-    name: 'survey',
-    path: '/:locale/survey/?',
-    handler: require('../pages/survey.jsx')
-  },
   '/give-bitcoin/': {
     name: 'give-bitcoin',
     path: '/:locale/give-bitcoin/?',
     handler: require('../pages/give-bitcoin.jsx')
-  },
-  '/sequential/': {
-    name: 'sequential',
-    path: '/:locale/sequential/?',
-    handler: require('../pages/sequential.jsx')
-  },
-  '/paypal-donate/': {
-    name: 'paypal-donate',
-    path: '/:locale/paypal-donate/?',
-    handler: require('../pages/paypal-donate.jsx')
-  },
-  '/directory-tiles/': {
-    name: 'directory-tiles',
-    path: '/:locale/directory-tiles/?',
-    handler: require('../pages/directory-tiles.jsx')
   },
   '/thunderbird/': {
     name: 'thunderbird-onepage',

33f4a093a044dac4a4f4b2b9065f8cfb57847806
ScottDowne <scott.downe@gmail.com>
2016-03-23 15:53:56 -0400

    adding share message to thunderbird share page

---
+++ components/thunderbird/social.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+module.exports = React.createClass({
+  mixins: [require('react-intl').IntlMixin],
+  render: function() {
+    var appURL = process.env.APPLICATION_URI;
+    var twitterShareURL = 'https://twitter.com/share?url=' + appURL +'/' + this.props.language + '/thunderbird/&text=' + encodeURIComponent(this.getIntlMessage('i_donated_to_thunderbird'));
+    var facebookShareURL = 'https://www.facebook.com/sharer/sharer.php?u=' + appURL + '/' + this.props.language + '/thunderbird/';
+    return (
+      <div className="share-page">
+        <div className="container">
+          <h3>
+            <div>{this.getIntlMessage('tell_your_friends')}</div>
+            <div>{this.getIntlMessage('help_spread_the_word')}</div>
+          </h3>
+          <div className="row">
+            <div className="half" id="facebook">
+              <div className="social-button"></div>
+              <a href={facebookShareURL} target="_blank">
+                <i className="fa fa-facebook fa-4x"></i>
+                <div>facebook</div>
+              </a>
+            </div>
+            <div className="half" id="twitter">
+              <div className="social-button"></div>
+              <a href={twitterShareURL} target="_blank">
+                <i className="fa fa-twitter fa-5x"></i>
+                <div>twitter</div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+});
--- scripts/route-file-content.js
+++ scripts/route-file-content.js
@@ -15,6 +15,9 @@ module.exports = function(outputPath, callback) {
     var currencyCode = localeCurrencyData[locale] || 'usd';
     var country = localeCountryData[locale] || 'US';
     var favicon = "/assets/images/favicon.8af3a74ede48e250ceb935c026242483.ico";
+    var twitterImage = "/assets/images/EOY_Twitter_v8_EN.d1bb5d2a5ce35859d038df852d9e6a0a811beaac.png"
+    var facebookImage = "/assets/images/EOY_facebook_v1.a152496406bad899d1a920f6d6b9f507.png"
+
     var values = {
       currency: currencies[currencyCode],
       presets: currencies.usd.presets.single,
@@ -25,9 +28,6 @@ module.exports = function(outputPath, callback) {
     var index = React.createFactory(require('../pages/index.jsx'));
     var page = React.createFactory(Handler);
     var currentStrings, mergedStrings;
-    if (outputPath.indexOf('thunderbird') !== -1) {
-      favicon = "/assets/images/thunderbird/favicon.ico";
-    }
     if (locale && locales[locale]) {
       currentStrings = locales[locale];
       mergedStrings = Object.assign({}, englishStrings, currentStrings);
@@ -36,18 +36,26 @@ module.exports = function(outputPath, callback) {
       locale = 'en-US';
       values = Object.assign({}, {locales : [locale], messages: englishStrings}, values);
     }
+    var desc = values.messages.i_donated_to_mozilla;
+    if (outputPath.indexOf('thunderbird') !== -1) {
+      favicon = "/assets/images/thunderbird/favicon.ico";
+      desc = values.messages.i_donated_to_thunderbird;
+      twitterImage = "/assets/images/thunderbird/TorontoSummit2014m.12923cab901787ca8681718646196167.jpg"
+      facebookImage = "/assets/images/thunderbird/TorontoSummit2014m.12923cab901787ca8681718646196167.jpg"
+    }
     FS.makeTree(Path.join(__dirname, '..', 'public', outputPath)).then(function() {
       var contentOfTheFile = React.renderToStaticMarkup(index({
         localeInfo: locale,
         favicon,
         metaData: {
           current_url: outputPath,
-          desc: values.messages.i_donated_to_mozilla,
+          desc: desc,
           title: values.messages.support_mozilla,
           site_name: 'mozilla.org',
           site_url: url.resolve(process.env.APPLICATION_URI, locale + '/'),
           site_title: values.messages.give_to_mozilla,
-          APPLICATION_URI: process.env.APPLICATION_URI
+          facebook_image: process.env.APPLICATION_URI + facebookImage,
+          twitter_image: process.env.APPLICATION_URI + twitterImage
         },
         markup: React.renderToString(page(values))
       }));

9baf69d1ebebb21b40bd830e75810a5cb6c719b0
Rebecca Lam <rebeccal@mozillafoundation.org>
2016-11-18 16:57:05 -0500

    Design changes to donate.mozilla.org thank you and share pages reference github ticket#623

--- src/components/social.js
+++ src/components/social.js
@@ -9,7 +9,8 @@ module.exports = React.createClass({
     var locale = this.context.intl.locale;
     var twitterShareURL = 'https://twitter.com/share?url=' + appURL +'/' + locale + '/&text=' + encodeURIComponent(this.context.intl.formatMessage({id: 'i_donated_to_mozilla'}));
     var facebookShareURL = 'https://www.facebook.com/sharer/sharer.php?u=' + appURL + '/' + locale + '/';
-    return (
+    var emailShareURL = 'mailto:someone@example.com?subject='+ emailSubject +'&body='+ emailBody +'';
+    return (""
       <div className="share-page">
         <div className="container">
           <h3>
@@ -17,18 +18,25 @@ module.exports = React.createClass({
             <div>{this.context.intl.formatMessage({id: 'help_spread_the_word'})}</div>
           </h3>
           <div className="row">
-            <div className="half" id="facebook">
+            <div className="third" id="facebook">
               <div className="social-button"></div>
               <a href={facebookShareURL} target="_blank">
-                <i className="fa fa-facebook fa-4x"></i>
+                <i className="fa fa-facebook fa-2x"></i>
                 <div>facebook</div>
               </a>
             </div>
-            <div className="half" id="twitter">
+            <div className="third" id="twitter">
               <div className="social-button"></div>
               <a href={twitterShareURL} target="_blank">
-                <i className="fa fa-twitter fa-5x"></i>
+                <i className="fa fa-twitter fa-2x"></i>
                 <div>twitter</div>
+              </a>
+            </div>
+            <div className="third" id="email">
+              <div className="social-button"></div>
+              <a href={emailShareURL} target="_blank">
+              <i className="fa fa-envelope fa-2x"></i>
+                <div>email</div>
               </a>
             </div>
           </div>

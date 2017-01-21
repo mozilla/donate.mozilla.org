25a6b11743ca151a16c5459fae2d3efc2459641b
Théo Chevalier <theo.chevalier11@gmail.com>
2017-01-21 04:47:49 +0000

    Paypal locale codes are pretty busted - fixes #1738

--- intl-config.js
+++ intl-config.js
@@ -2,11 +2,27 @@ var intlConfig = {
   "dest": "public",
   "src": "locales",
   "paypalLocales": {
-    "en-US": "US",
+    "ar": "ar_EG",
     "de": "DE",
+    "da": "da_DK",
+    "es": "ES",
+    "en-GB": "en_GB",
+    "en-US": "en_US",
     "fr": "FR",
+    "he": "he_IL",
+    "id": "id_ID",
+    "it": "it_IT",
+    "ja": "ja_JP",
+    "ko": "ko_KR",
+    "nb-NO": "no_NO",
+    "nl": "nl_NL",
+    "nn-NO": "no_NO",
     "pt-BR": "BR",
-    "id": "id_ID"
+    "ru": "RU",
+    "sv-SE": "sv_SE",
+    "th": "th_TH",
+    "zh-CN": "zh_CN",
+    "zh-TW": "zh_TW",
   }
 };
 

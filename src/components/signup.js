4ad14d69f411dc83d911cc06954eeb20711d179b
ScottDowne <scott.downe@gmail.com>
2017-01-23 14:47:08 -0500

    Removing dead components, and removing country input, country test and related files.

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Email from './email-input.js';
 import {PrivacyPolicyCheckbox} from './checkbox.js';
-import {Country} from './address-input.js';
 import SubmitButton from './submit-button.js';
 
 var SIGNUP_SUBMITTING = 1;
@@ -23,32 +22,16 @@ var Signup = React.createClass({
           <div className="wrap">
             <div className="row">
               <Email name="email"/>
-              <div className="full country-signup">
-                <Country name="country"/>
-              </div>
               <PrivacyPolicyCheckbox name="privacyPolicy"/>
-              <div className="no-country-signup">
-                <SubmitButton
-                  submitting={this.state.submitting === SIGNUP_SUBMITTING}
-                  validate={["email", "privacyPolicy"]}
-                  onSubmit={this.signup}
-                  submit={["email"]}
-                  errors={["email"]}
-                >
-                  {this.context.intl.formatMessage({id: 'sign_up_now'})}
-                </SubmitButton>
-              </div>
-              <div className="country-signup">
-                <SubmitButton
-                  submitting={this.state.submitting === SIGNUP_SUBMITTING}
-                  validate={["email", "privacyPolicy"]}
-                  onSubmit={this.signup}
-                  submit={["email", "country"]}
-                  errors={["email", "country"]}
-                >
-                  {this.context.intl.formatMessage({id: 'sign_up_now'})}
-                </SubmitButton>
-              </div>
+              <SubmitButton
+                submitting={this.state.submitting === SIGNUP_SUBMITTING}
+                validate={["email", "privacyPolicy"]}
+                onSubmit={this.signup}
+                submit={["email"]}
+                errors={["email"]}
+              >
+                {this.context.intl.formatMessage({id: 'sign_up_now'})}
+              </SubmitButton>
             </div>
           </div>
         </div>
--- src/components/thunderbird/signup.js
+++ src/components/thunderbird/signup.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Email from '../email-input.js';
 import {PrivacyPolicyCheckbox} from '../checkbox.js';
-import {Country} from '../address-input.js';
 import SubmitButton from '../submit-button.js';
 
 var SIGNUP_SUBMITTING = 1;
@@ -22,32 +21,16 @@ var Signup = React.createClass({
           <div className="wrap">
             <div className="row">
               <Email name="email"/>
-              <div className="full country-signup">
-                <Country name="country"/>
-              </div>
               <PrivacyPolicyCheckbox name="privacyPolicy" intlId='privacy_policy_thunderbird'/>
-              <div className="no-country-signup">
-                <SubmitButton
-                  submitting={this.state.submitting === SIGNUP_SUBMITTING}
-                  validate={["email", "privacyPolicy"]}
-                  onSubmit={this.mailchimp}
-                  submit={["email"]}
-                  errors={["email"]}
-                >
-                  {this.context.intl.formatMessage({id: 'sign_up_now'})}
-                </SubmitButton>
-              </div>
-              <div className="country-signup">
-                <SubmitButton
-                  submitting={this.state.submitting === SIGNUP_SUBMITTING}
-                  validate={["email", "privacyPolicy"]}
-                  onSubmit={this.mailchimp}
-                  submit={["email", "country"]}
-                  errors={["email", "country"]}
-                >
-                  {this.context.intl.formatMessage({id: 'sign_up_now'})}
-                </SubmitButton>
-              </div>
+              <SubmitButton
+                submitting={this.state.submitting === SIGNUP_SUBMITTING}
+                validate={["email", "privacyPolicy"]}
+                onSubmit={this.mailchimp}
+                submit={["email"]}
+                errors={["email"]}
+              >
+                {this.context.intl.formatMessage({id: 'sign_up_now'})}
+              </SubmitButton>
             </div>
           </div>
         </div>

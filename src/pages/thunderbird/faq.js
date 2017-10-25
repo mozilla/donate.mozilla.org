e5acf6b8f675f7e283b8a57c95d714bb37953308
Théo Chevalier <theo.chevalier11@gmail.com>
2017-10-25 12:41:27 +0100

    Enable new FAQ templates for all locales

--- src/pages/thunderbird/faq.js
+++ src/pages/thunderbird/faq.js
@@ -87,205 +87,6 @@ var Faq = React.createClass({
     }
     var activeKey = this.state.activeKey;
 
-    // FIXME: Temporary expose the new template to English and locales that are ready,
-    // until we have a good coverage.
-    if (/^(en|cs|de|es|fr|it|nl|pl|pt-BR|ru|sv-SE|zh-CN|zh-TW)(\b|$)/.test(this.context.intl.locale)) {
-      return (
-        <div className={className}>
-          <div className="faq-header">
-            <div className="container">
-              <h1><FormattedHTMLMessage id='faq_header'/></h1>
-              <Link to={'/' + this.context.intl.locale + '/thunderbird'}>
-                <FormattedHTMLMessage id='faq_donate_link'/>
-              </Link>
-            </div>
-          </div>
-          <div className="container">
-            <h2>
-              <FormattedHTMLMessage id='faq_intro_shorter_thunderbird'/>
-            </h2>
-            <div className="collapse">
-
-              <Panel activeKey={activeKey} activateKey={this.onKeyChange} itemKey="item_1" header={this.context.intl.formatHTMLMessage({id: 'faq_item_1_header'})}>
-                <p><FormattedHTMLMessage id='faq_item_1_paragraph_wire_thunderbird'/></p>
-              </Panel>
-
-              <Panel activeKey={activeKey} activateKey={this.onKeyChange} itemKey="item_2" header={this.context.intl.formatHTMLMessage({id: 'faq_item_2_header_thunderbird'})}>
-                <p>
-                  <FormattedHTMLMessage id='sepa_desc' />
-                  {" "}
-                  <FormattedHTMLMessage id='wire_transfer_cost_info2' />
-                </p>
-                <h4><FormattedHTMLMessage id='euro_title' /></h4>
-                <p>
-                  <b><FormattedHTMLMessage id='account_currency' /></b>
-                  {" "}
-                  <FormattedHTMLMessage id='euro_currency' />
-                  <br/>
-                  <b><FormattedHTMLMessage id='receiving_bank' /></b>
-                  {" Standard Chartered Bank"}
-                  <br/>
-                  <b><FormattedHTMLMessage id='city_and_country' /></b>
-                  {" "}
-                  <FormattedHTMLMessage id='frankfurt' />
-                  <br/>
-                  <b><FormattedHTMLMessage id='swift_code' /></b>
-                  {" SCBLDEFX"}
-                  <br/>
-                  <b><FormattedHTMLMessage id='beneficiary' /></b>
-                  {" SVB-Mozilla Foundation"}
-                  <br/>
-                  <b><FormattedHTMLMessage id='IBAN' /></b>
-                  {" DE67512305000500136802"}
-                  <br/>
-                  <b><FormattedHTMLMessage id='memo_field' /></b>
-                  {" Thunderbird"}
-                </p>
-                <br/>
-                <h4><FormattedHTMLMessage id='gbp_title' /></h4>
-                <p>
-                  <b><FormattedHTMLMessage id='account_currency' /></b>
-                  {" "}
-                  <FormattedHTMLMessage id='gbp_currency' />
-                  <br/>
-                  <b><FormattedHTMLMessage id='receiving_bank' /></b>
-                  {" National Westminster Bank"}
-                  <br/>
-                  <b><FormattedHTMLMessage id='city_and_country' /></b>
-                  {" "}
-                  <FormattedHTMLMessage id='london' />
-                  <br/>
-                  <b><FormattedHTMLMessage id='sort_code' /></b>
-                  {" 60-00-04"}
-                  <br/>
-                  <b><FormattedHTMLMessage id='account_number' /></b>
-                  {" 10017496"}
-                  <br/>
-                  <b><FormattedHTMLMessage id='beneficiary' /></b>
-                  {" SVB RE Mozilla Foundation"}
-                  <br/>
-                  <b><FormattedHTMLMessage id='memo_field' /></b>
-                  {" Thunderbird"}
-                </p>
-                <p>
-                  <b><FormattedHTMLMessage id='swift_code' /></b>
-                  {" NWBKGB2L"}
-                  <br/>
-                  <b><FormattedHTMLMessage id='IBAN' /></b>
-                  {" GB77NWBK60000410017496"}
-                </p>
-                <p>
-                  <FormattedHTMLMessage
-                    id='physical_address'
-                    values={{
-                      MozillaMV: `331 E. Evelyn Ave, Mountain View, CA 94041 USA`
-                    }}
-                  />
-                </p>
-                <br/>
-                <h4><FormattedHTMLMessage id='usd_title' /></h4>
-                <p>
-                  <b><FormattedHTMLMessage id='receiving_bank' /></b>
-                  {" Silicon Valley Bank"}
-                  <br/>
-                  <b><FormattedHTMLMessage id='bank_address' /></b>
-                  {" 3003 Tasman Drive, Santa Clara, CA 94054 USA"}
-                </p>
-                <h6><FormattedHTMLMessage id='domestic_transfer' /></h6>
-                <b><FormattedHTMLMessage id='routing_transit' /></b>
-                {" 121140399"}
-                <br/>
-                <b><FormattedHTMLMessage id='beneficiary' /></b>
-                {" Mozilla Foundation"}
-                <br/>
-                <b><FormattedHTMLMessage id='account_number_usa' /></b>
-                {" 3302228513"}
-                <br/>
-                <b><FormattedHTMLMessage id='memo_field' /></b>
-                {" Thunderbird"}
-                <br/><br/>
-                <h6><FormattedHTMLMessage id='international_transfer' /></h6>
-                <b><FormattedHTMLMessage id='routing_transit' /></b>
-                {" 121140399"}
-                <br/>
-                <b><FormattedHTMLMessage id='beneficiary' /></b>
-                {" Mozilla Foundation"}
-                <br/>
-                <b><FormattedHTMLMessage id='account_number_usa' /></b>
-                {" 3302228513"}
-                <br/>
-                <b><FormattedHTMLMessage id='memo_field' /></b>
-                {" Thunderbird"}
-                <br/>
-                <b><FormattedHTMLMessage id='swift_code' /></b>
-                {" SVBKUS6S"}
-                <br/><br/>
-              </Panel>
-
-              <Panel activeKey={activeKey} activateKey={this.onKeyChange} itemKey="item_3" header={this.context.intl.formatHTMLMessage({id: 'faq_item_3_header'})}>
-                <p><FormattedHTMLMessage id='faq_item_3_paragraph_thunderbird'/></p>
-              </Panel>
-
-              <Panel activeKey={activeKey} activateKey={this.onKeyChange} itemKey="item_4" header={this.context.intl.formatHTMLMessage({id: 'faq_item_4_header_check'})}>
-
-                {this.renderRowCheckInstructions()}
-                <address>
-                  Thunderbird at Mozilla Foundation
-                  <br/>
-                  331 East Evelyn Avenue,
-                  <br/>
-                  Mountain View, CA 94041
-                  <br/>
-                  USA
-                </address>
-                <p><FormattedHTMLMessage id='faq_item_4_paragraph_h_thunderbird'/></p>
-              </Panel>
-
-              <Panel activeKey={activeKey} activateKey={this.onKeyChange} itemKey="item_5" header={this.context.intl.formatHTMLMessage({id: 'faq_item_5_header'})}>
-                <p><FormattedHTMLMessage id='faq_item_5_paragraph_thunderbird'/></p>
-              </Panel>
-
-              <Panel activeKey={activeKey} activateKey={this.onKeyChange} itemKey="item_6" header={this.context.intl.formatHTMLMessage({id: 'faq_item_6_header_thunderbird'})}>
-                <p><FormattedHTMLMessage id='faq_item_6_paragraph_a_thunderbird'/></p>
-              </Panel>
-
-              <Panel activeKey={activeKey} activateKey={this.onKeyChange} itemKey="item_7" header={this.context.intl.formatHTMLMessage({id: 'faq_item_7_header'})}>
-                <p><FormattedHTMLMessage id='faq_item_7_paragraph_a_thunderbird'/></p>
-              </Panel>
-
-              <Panel activeKey={activeKey} activateKey={this.onKeyChange} itemKey="item_8" header={this.context.intl.formatHTMLMessage({id: 'faq_item_8_header_thunderbird'})}>
-                <p><FormattedHTMLMessage id='faq_item_8_paragraph_a_thunderbird'/></p>
-              </Panel>
-
-              <Panel activeKey={activeKey} activateKey={this.onKeyChange} itemKey="item_9" header={this.context.intl.formatHTMLMessage({id: 'faq_item_9_header_thunderbird'})}>
-                <p><FormattedHTMLMessage id='faq_item_9_paragraph_thunderbird'/></p>
-              </Panel>
-
-              <Panel activeKey={activeKey} activateKey={this.onKeyChange} itemKey="item_10" header={this.context.intl.formatHTMLMessage({id: 'faq_item_10_header'})}>
-                <p><FormattedHTMLMessage id='faq_item_10_paragraph_thunderbird'/></p>
-              </Panel>
-
-              <Panel activeKey={activeKey} activateKey={this.onKeyChange} itemKey="item_tax_a" header={this.context.intl.formatHTMLMessage({id: 'faq_item_tax_header'})}>
-                <p><FormattedHTMLMessage id='donation_notice'/> <FormattedHTMLMessage id='faq_item_tax_paragraph_a_tax_id'/></p>
-                <p><FormattedHTMLMessage id='faq_item_tax_paragraph_b'/></p>
-                <p><FormattedHTMLMessage id='faq_item_tax_paragraph_c'/></p>
-              </Panel>
-
-              {this.renderEnglishTaxItem()}
-
-              <Panel activeKey={activeKey} activateKey={this.onKeyChange} itemKey="item_11" header={this.context.intl.formatHTMLMessage({id: 'faq_item_11_header'})}>
-                <p><FormattedHTMLMessage id='faq_item_11_paragraph_thunderbird'/></p>
-              </Panel>
-
-              <Panel activeKey={activeKey} activateKey={this.onKeyChange} itemKey="item_15" header={this.context.intl.formatHTMLMessage({id: 'faq_item_15_header'})}>
-                <p><FormattedHTMLMessage id='faq_item_15_paragraph_thunderbird'/></p>
-              </Panel>
-            </div>
-          </div>
-          <MozillaFooter/>
-        </div>
-      );
-    }
     return (
       <div className={className}>
         <div className="faq-header">
@@ -298,25 +99,133 @@ var Faq = React.createClass({
         </div>
         <div className="container">
           <h2>
-            <FormattedHTMLMessage id='faq_intro_thunderbird'/>
+            <FormattedHTMLMessage id='faq_intro_shorter_thunderbird'/>
           </h2>
           <div className="collapse">
 
             <Panel activeKey={activeKey} activateKey={this.onKeyChange} itemKey="item_1" header={this.context.intl.formatHTMLMessage({id: 'faq_item_1_header'})}>
-              <p><FormattedHTMLMessage id='faq_item_1_paragraph_thunderbird'/></p>
+              <p><FormattedHTMLMessage id='faq_item_1_paragraph_wire_thunderbird'/></p>
             </Panel>
 
             <Panel activeKey={activeKey} activateKey={this.onKeyChange} itemKey="item_2" header={this.context.intl.formatHTMLMessage({id: 'faq_item_2_header_thunderbird'})}>
-              <p><FormattedHTMLMessage id='faq_item_2_p_thunderbird_a'/></p>
-              <p><FormattedHTMLMessage id='faq_item_2_p_thunderbird_b'/></p>
+              <p>
+                <FormattedHTMLMessage id='sepa_desc' />
+                {" "}
+                <FormattedHTMLMessage id='wire_transfer_cost_info2' />
+              </p>
+              <h4><FormattedHTMLMessage id='euro_title' /></h4>
+              <p>
+                <b><FormattedHTMLMessage id='account_currency' /></b>
+                {" "}
+                <FormattedHTMLMessage id='euro_currency' />
+                <br/>
+                <b><FormattedHTMLMessage id='receiving_bank' /></b>
+                {" Standard Chartered Bank"}
+                <br/>
+                <b><FormattedHTMLMessage id='city_and_country' /></b>
+                {" "}
+                <FormattedHTMLMessage id='frankfurt' />
+                <br/>
+                <b><FormattedHTMLMessage id='swift_code' /></b>
+                {" SCBLDEFX"}
+                <br/>
+                <b><FormattedHTMLMessage id='beneficiary' /></b>
+                {" SVB-Mozilla Foundation"}
+                <br/>
+                <b><FormattedHTMLMessage id='IBAN' /></b>
+                {" DE67512305000500136802"}
+                <br/>
+                <b><FormattedHTMLMessage id='memo_field' /></b>
+                {" Thunderbird"}
+              </p>
+              <br/>
+              <h4><FormattedHTMLMessage id='gbp_title' /></h4>
+              <p>
+                <b><FormattedHTMLMessage id='account_currency' /></b>
+                {" "}
+                <FormattedHTMLMessage id='gbp_currency' />
+                <br/>
+                <b><FormattedHTMLMessage id='receiving_bank' /></b>
+                {" National Westminster Bank"}
+                <br/>
+                <b><FormattedHTMLMessage id='city_and_country' /></b>
+                {" "}
+                <FormattedHTMLMessage id='london' />
+                <br/>
+                <b><FormattedHTMLMessage id='sort_code' /></b>
+                {" 60-00-04"}
+                <br/>
+                <b><FormattedHTMLMessage id='account_number' /></b>
+                {" 10017496"}
+                <br/>
+                <b><FormattedHTMLMessage id='beneficiary' /></b>
+                {" SVB RE Mozilla Foundation"}
+                <br/>
+                <b><FormattedHTMLMessage id='memo_field' /></b>
+                {" Thunderbird"}
+              </p>
+              <p>
+                <b><FormattedHTMLMessage id='swift_code' /></b>
+                {" NWBKGB2L"}
+                <br/>
+                <b><FormattedHTMLMessage id='IBAN' /></b>
+                {" GB77NWBK60000410017496"}
+              </p>
+              <p>
+                <FormattedHTMLMessage
+                  id='physical_address'
+                  values={{
+                    MozillaMV: `331 E. Evelyn Ave, Mountain View, CA 94041 USA`
+                  }}
+                />
+              </p>
+              <br/>
+              <h4><FormattedHTMLMessage id='usd_title' /></h4>
+              <p>
+                <b><FormattedHTMLMessage id='receiving_bank' /></b>
+                {" Silicon Valley Bank"}
+                <br/>
+                <b><FormattedHTMLMessage id='bank_address' /></b>
+                {" 3003 Tasman Drive, Santa Clara, CA 94054 USA"}
+              </p>
+              <h6><FormattedHTMLMessage id='domestic_transfer' /></h6>
+              <b><FormattedHTMLMessage id='routing_transit' /></b>
+              {" 121140399"}
+              <br/>
+              <b><FormattedHTMLMessage id='beneficiary' /></b>
+              {" Mozilla Foundation"}
+              <br/>
+              <b><FormattedHTMLMessage id='account_number_usa' /></b>
+              {" 3302228513"}
+              <br/>
+              <b><FormattedHTMLMessage id='memo_field' /></b>
+              {" Thunderbird"}
+              <br/><br/>
+              <h6><FormattedHTMLMessage id='international_transfer' /></h6>
+              <b><FormattedHTMLMessage id='routing_transit' /></b>
+              {" 121140399"}
+              <br/>
+              <b><FormattedHTMLMessage id='beneficiary' /></b>
+              {" Mozilla Foundation"}
+              <br/>
+              <b><FormattedHTMLMessage id='account_number_usa' /></b>
+              {" 3302228513"}
+              <br/>
+              <b><FormattedHTMLMessage id='memo_field' /></b>
+              {" Thunderbird"}
+              <br/>
+              <b><FormattedHTMLMessage id='swift_code' /></b>
+              {" SVBKUS6S"}
+              <br/><br/>
             </Panel>
 
             <Panel activeKey={activeKey} activateKey={this.onKeyChange} itemKey="item_3" header={this.context.intl.formatHTMLMessage({id: 'faq_item_3_header'})}>
               <p><FormattedHTMLMessage id='faq_item_3_paragraph_thunderbird'/></p>
             </Panel>
 
-            <Panel activeKey={activeKey} activateKey={this.onKeyChange} itemKey="item_4" header={this.context.intl.formatHTMLMessage({id: 'faq_item_4_header'})}>
-              <p><FormattedHTMLMessage id='faq_item_4_paragraph_a_thunderbird'/></p>
+            <Panel activeKey={activeKey} activateKey={this.onKeyChange} itemKey="item_4" header={this.context.intl.formatHTMLMessage({id: 'faq_item_4_header_check'})}>
+
+              {this.renderRowCheckInstructions()}
               <address>
                 Thunderbird at Mozilla Foundation
                 <br/>

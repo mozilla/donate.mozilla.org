e130b53b2fbcbd3a83df4e2fd5fe03125f40f044
Magnus Melin <mkmelin+github@iki.fi>
2017-11-12 22:33:12 +0200

    Add check and wire transfer links to the front page.

--- src/components/thunderbird/small-print.js
+++ src/components/thunderbird/small-print.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import { FormattedHTMLMessage } from 'react-intl';
+import Link from './../link.js';
+import { FormattedHTMLMessage, FormattedMessage } from 'react-intl';
 
 var Footer = React.createClass({
   contextTypes: {
     intl: React.PropTypes.object
   },
   render: function() {
+    var wireTransferLink = (<Link to={'/' + this.context.intl.locale + '/thunderbird/faq#item_2'}>{this.context.intl.formatMessage({id: 'wireTransfer'})}</Link>);
+    var checkLink = (<a href={'/' + this.context.intl.locale + '/thunderbird/faq#item_4'}>{this.context.intl.formatMessage({id: 'check'})}</a>);
     return (
       <div className="row disclaimers">
+        <p className="other-ways-to-give">
+          <FormattedMessage
+            id='other_way_to_give_wire_check'
+            values={{
+              wireTransferLink,
+              checkLink
+            }}
+          />
+        </p>
         <p className="need-help">
           <FormattedHTMLMessage id="problems_donating_thunderbird2"/>
         </p>

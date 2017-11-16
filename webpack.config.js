31776857e8316f6133e12f4984282574c4c45cae
Pomax <pomax@nihongoresources.com>
2017-11-16 13:17:29 -0800

    SEPA feature flag

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,7 @@ module.exports = {
       'process.env': JSON.stringify({
         APPLICATION_URI: process.env.APPLICATION_URI,
         STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY,
+        SEPA_ENABLED: process.env.SEPA_ENABLED,
         OPTIMIZELY_ID: process.env.OPTIMIZELY_ID,
         OPTIMIZELY_ACTIVE: process.env.OPTIMIZELY_ACTIVE,
         FULL_SUBDOMAIN_FOR_COOKIE: process.env.FULL_SUBDOMAIN_FOR_COOKIE,

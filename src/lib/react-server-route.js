3d940118a8371b413e4ff3c278e4e6de3235a235
Théo Chevalier <theo.chevalier11@gmail.com>
2017-12-21 21:15:48 +0100

    Localized twitter card (#1942)
    * Add fr + de twitter cards
    
    * Adding generic image
    
    * Remove extra var

--- src/lib/react-server-route.js
+++ src/lib/react-server-route.js
@@ -30,12 +30,18 @@ function routeFileContent(locales) {
     function generateHTML(renderProps) {
       var messages = getMessages(locale);
       var favicon = "/assets/images/favicon.d0608f227db61f2852a32087e614911c.png";
-      var twitterImage = "/assets/images/twitter-card.png";
+      var twitterImage = "/assets/images/twitter-card-generic.png";
       var facebookImage = "/assets/images/og.jpg";
       var siteUrl = locale + '/';
       var localesInfo = [locale];
       var query = queryParser(request.query);
       var desc = messages.i_donated_to_mozilla_2017;
+      if (/^(en)(\b|$)/.test(locale)) {
+        twitterImage = "/assets/images/twitter-card-en.png";
+      }
+      if (/^(fr|de)(\b|$)/.test(locale)) {
+        twitterImage = "/assets/images/twitter-card-" + locale + ".png";
+      }
       if (location.indexOf('thunderbird') !== -1) {
         favicon = "/assets/images/thunderbird/favicon.ico";
         desc = messages.i_donated_to_thunderbird;

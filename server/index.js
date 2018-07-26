22e567d200656e11b95504b5c199892e6c074d11
Chris DeCairos <cade@mozillafoundation.org>
2018-07-26 17:30:30 -0400

    Redirect to the correct hostname

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
-require('habitat').load();
+const Habitat = require('habitat');
+Habitat.load();
+const env = new Habitat();
 
+const url = require('url');
 const Hapi = require('hapi');
 const AuthBearer = require('hapi-auth-bearer-token');
 const locales = require('./locales');
@@ -10,6 +13,7 @@ const services = require('./services');
 const finalizeServer = require('./finalise-server');
 
 const ONE_HOUR_MS = 1000 * 60 * 60;
+const appHost = url.parse(env.get(`APPLICATION_URI`)).host;
 
 module.exports = async function(options) {
   const serverOptions = getServerOptions(options);
@@ -20,7 +24,7 @@ module.exports = async function(options) {
   server.auth.strategy("stripe", "bearer-access-token", {
     allowQueryToken: true,
     validate: async(request, token, h) => {
-      const isValid = token === process.env.STRIPE_WEBHOOK_SECRET;
+      const isValid = token === env.get(`STRIPE_WEBHOOK_SECRET`);
       const credentials = { token: token };
       return { isValid, credentials };
     }
@@ -28,11 +32,30 @@ module.exports = async function(options) {
 
   server.state("session", {
     ttl: ONE_HOUR_MS,
-    isSecure: process.env.NODE_ENV === "production",
+    isSecure: env.get(`NODE_ENV`) === "production",
     encoding: "none"
   });
 
   server.route(baseRoutes);
+
+  if (env.get(`ENFORCE_HOSTNAME`)) {
+    server.ext('onRequest', (request, h) => {
+      let {host} = request.info;
+
+      if (appHost === host) {
+        return h.continue;
+      }
+
+      let parsed = url.parse(request.url.href, false);
+      let newURL = url.format({
+        protocol: parsed.protocol,
+        host: appHost,
+        pathname: parsed.path
+      });
+
+      return h.response().takeover().redirect(newURL);
+    });
+  }
 
   await server.register(services);
 

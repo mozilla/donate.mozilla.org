c0ba4c177652eec9f74472c60bceba3e99cc5b7f
Andrei Hajdukewycz <sancus@off.net>
2018-08-20 18:53:02 -0400

    Add download link banner to Thunderbird post-download donation page. (#2055)

--- src/pages/thunderbird/about.js
+++ src/pages/thunderbird/about.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FormattedHTMLMessage } from 'react-intl';
 import ThunderbirdFooter from '../../components/thunderbird/footer.js';
 import SingleForm from '../../components/single-form.js';
 
@@ -28,13 +29,18 @@ module.exports = React.createClass({
     });
   },
   render: function() {
+    var downloadBanner = "";
     var className = "row additional-info-container thunderbird";
     if (this.props.test) {
       className += " " + this.props.test;
     }
+    if (this.props.test === "tbdownload") {
+      downloadBanner = (<div className="download-failed"><h3><FormattedHTMLMessage id="thunderbird_download_banner" /></h3></div>);
+    }
     var aboutCopy = this.state.aboutCopy;
     return (
       <div className={className}>
+        {downloadBanner}
         <div className="additional-info-page">
           <div className="container additional-page">
             <img className="internet-graphic" width="224" src="/assets/images/thunderbird/thunderbird-logo-wordmark-small.png"/>

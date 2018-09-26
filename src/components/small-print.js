15dafdbad2400ac600b0015250ccf1c7b37bf908
Théo Chevalier <theo.chevalier11@gmail.com>
2018-09-26 18:16:15 +0100

    Update copy on homepage (fixes #2075)

--- src/components/small-print.js
+++ src/components/small-print.js
@@ -10,14 +10,18 @@ var Footer = React.createClass({
     var wireTransferLink = (<Link to={'/' + this.context.intl.locale + '/ways-to-give#wire'}>{this.context.intl.formatMessage({id: 'wireTransfer'})}</Link>);
     var checkLink = (<a href={'/' + this.context.intl.locale + '/ways-to-give#check'}>{this.context.intl.formatMessage({id: 'check'})}</a>);
     var privacyPolicyMessage = "privacy_policy_var_b";
+    var refundsMessage= "";
     if (this.props.frequency === "monthly") {
       privacyPolicyMessage = "privacy_policy_var_b_monthly";
+      refundsMessage = this.context.intl.formatMessage({id: 'privacy_policy_var_b_monthly_refund'});
     }
 
     return (
       <div className="row disclaimers">
         <p className="full">
           <FormattedHTMLMessage id={privacyPolicyMessage}/>
+          {` `}
+          <span>{refundsMessage}</span>
           {` `}
           <FormattedMessage
             id='other_way_to_give_wire_check'
